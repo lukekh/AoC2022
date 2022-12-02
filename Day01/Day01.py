c6d3d2a3b10a68400a0ec24c37b59e43d4f0e2e2
lukekh <luke.keating.hughes@gmail.com>
2022-12-02 22:59:52 +1030

    finished day 2

---
+++ Day01/Day01.py
@@ -0,0 +1,67 @@
+# Day01
+day = 1
+inputs = [i[:-1] for i in open('Day01.in').readlines()]
+
+
+# Part One
+def part_one(inputs):
+    # init
+    max_calories = 0
+    elf = 0
+    total = 0
+
+    # iterate over inputs
+    for row in inputs:
+        if row:
+            total += int(row)
+        else:
+            # update max
+            max_calories = max(max_calories, total)
+            max_elf = elf
+
+            # init next elf
+            elf += 1
+            total = 0
+    return max_calories, max_elf
+
+
+# Part Two
+def part_two(inputs):
+    # init
+    calories = []
+    total = 0
+
+    # iterate over inputs
+    for row in inputs:
+        if row:
+            total += int(row)
+        else:
+            # update max
+            calories.append(total)
+
+            # init next elf
+            total = 0
+    return sorted(calories)[-3:]
+
+
+# Main
+def main(inputs):
+    print(f":: Solutions to Day {day} ::")
+    # Sample of the inputs
+    print(f"Sample of inputs:")
+    print(inputs[0:10])
+
+    # Part One
+    print(f":: Part One ::")
+    c, e = part_one(inputs)
+    print(f"Elf {e} is carrying {c} total calories.")
+
+    # Part Two
+    print(f":: Part Two ::")
+    c = part_two(inputs)
+    print(f"The top three calorie counts are {', '.join([str(i) for i in c])} hence the total is {sum(c)} calories.")
+
+
+# Run main
+if __name__ == "__main__":
+    main(inputs)
---
+++ Day02/Day02.py
@@ -0,0 +1,65 @@
+# Day02
+# Here is my solution to Day 2
+import time
+t = time.time()
+day = 2
+inputs = [i[:-1].split(" ") for i in open('Day02.in').readlines()]
+
+
+# Helper functions
+score_modifier = {
+    "X": 1,
+    "Y": 2,
+    "Z": 3,
+}
+
+def rps(p1, p2, modifier=score_modifier):
+    lose = [("A", "Z"), ("B", "X"), ("C", "Y"),]
+    draw = [("A", "X"), ("B", "Y"), ("C", "Z"),]
+    if (p1, p2) in lose:
+        return 0 + modifier[p2]
+    elif (p1, p2) in draw:
+        return 3 + modifier[p2]
+    else:
+        return 6 + modifier[p2]
+
+
+# Part One
+def part_one(inputs):
+    return sum(
+        [rps(*i) for i in inputs]
+    )
+
+def strat(p1, result):
+    modifier = {"A": 1, "B": 2, "C": 3}
+    plays = {
+        "X": {"A": "C", "B": "A", "C": "B"},
+        "Y": {"A": "A", "B": "B", "C": "C"},
+        "Z": {"A": "B", "B": "C", "C": "A"},
+    }
+    results = {"X": 0, "Y": 3, "Z": 6}
+
+    return results[result] + modifier[plays[result][p1]]
+
+# Part Two
+def part_two(inputs):
+    return sum(
+        [strat(*i) for i in inputs]
+    )
+
+# Main
+def main(inputs):
+    print(f":: Solutions to Day {day} ::")
+    
+    # Part One
+    print(f":: Part One ::")
+    print(f"Your score would be {part_one(inputs)} if everything goes to plan.")
+
+    # Part Two
+    print(f":: Part Two ::")
+    print(f"Your score would be {part_two(inputs)} if everything goes to the strategy guide.")
+
+
+# Run main
+if __name__ == "__main__":
+    main(inputs)
